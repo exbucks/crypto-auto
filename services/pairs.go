e4ebd37886d6fae8f077171cebed9e78cc2dd970
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-19 12:43:45 +0900

    update console

--- services/pairs.go
+++ services/pairs.go
@@ -36,7 +36,8 @@ func TrackPairs() {
 
 			systray.SetTitle(fmt.Sprintf("%s %s", n, price))
 			systray.SetTooltip("Crypto Auto")
-			fmt.Println("---->>>  ", n, change, duration, a)
+			t := time.Now()
+			fmt.Println(t.Format("2006/01/02 15:04:05"), ": ", n, change, duration, a)
 
 			if p != olds[0] {
 				message := fmt.Sprintf("%s: %s %s %s", n, price, change, duration)
