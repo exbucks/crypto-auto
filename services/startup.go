e741d73cdb1ab044a95d1c9abf7288c8a02e8f6a
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-21 22:57:40 +0900

    clean up code base

--- services/startup.go
+++ services/startup.go
@@ -38,10 +38,14 @@ func Startup(command <-chan string, alert float64) {
 }
 
 func trackMainPair() {
+	address := os.Getenv("MAIN_PAIR")
+	trackOnePair(address)
+}
+
+func trackOnePair(address string) {
 	money := accounting.Accounting{Symbol: "$", Precision: 6}
 	cc := make(chan string, 1)
 	var swaps utils.Swaps
-	address := os.Getenv("MAIN_PAIR")
 	go utils.SwapsByCounts(cc, 2, address)
 
 	msg := <-cc
