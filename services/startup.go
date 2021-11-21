9f885c5c78f0a5c3dbf357386f814540d8d9b4fa
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-22 00:22:01 +0900

    clean up code base

--- services/startup.go
+++ services/startup.go
@@ -13,6 +13,18 @@ import (
 )
 
 var oldPrices = map[string]float64{}
+var minPrices = map[string]float64{
+	"0x7a99822968410431edd1ee75dab78866e31caf39": 0.4,
+	"0x22527f92f43dc8bea6387ce40b87ebaa21f51df3": 1.0,
+	"0x684b00a5773679f88598a19976fbeb25a68e9a5f": 0.4,
+	"0xc88ac988a655b91b70def427c8778b4d43f2048d": 0.6,
+}
+var maxPrices = map[string]float64{
+	"0x7a99822968410431edd1ee75dab78866e31caf39": 0.6,
+	"0x22527f92f43dc8bea6387ce40b87ebaa21f51df3": 2.0,
+	"0x684b00a5773679f88598a19976fbeb25a68e9a5f": 0.6,
+	"0xc88ac988a655b91b70def427c8778b4d43f2048d": 0.75,
+}
 
 func Startup(command <-chan string, alert float64) {
 	var status = "Play"
@@ -77,14 +89,19 @@ func trackOnePair(address string, target string) {
 			title = "Price changed down!"
 		}
 		link := fmt.Sprintf("https://www.dextools.io/app/ether/pair-explorer/%s", address)
+		var sound gosxnotifier.Sound
 		if target == "main" {
 			systray.SetTitle(fmt.Sprintf("%s|%f", n, p))
-			Notify(title, message, link, gosxnotifier.Default)
+			sound = gosxnotifier.Default
 		} else {
-			Notify(title, message, link, gosxnotifier.Glass)
+			sound = gosxnotifier.Morse
 		}
+		if p < minPrices[address] || p > maxPrices[address] {
+			sound = gosxnotifier.Basso
+		}
+		Notify(title, message, link, sound)
 		fmt.Println(".")
-		fmt.Println(t.Format("2006/01/02 15:04:05"), ": ", n, price, change, duration, a)
+		fmt.Println(t.Format("2006/01/02 15:04:05"), ": ", n, price, change, duration)
 		fmt.Println(".")
 	}
 	oldPrices[address] = p
