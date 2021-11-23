39926c315e88fbec044f7ee93ef3b89c4d417d7c
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-23 14:22:25 +0900

    clean up code base

--- services/core.go
+++ services/core.go
@@ -1,28 +1,14 @@
 package services
 
 import (
-	"log"
 	"runtime"
 
 	gosxnotifier "github.com/deckarep/gosx-notifier"
-	"gopkg.in/toast.v1"
 )
 
 func Notify(title string, message string, link string, sound gosxnotifier.Sound) {
 	if runtime.GOOS == "windows" {
-		notification := toast.Notification{
-			AppID:   "crypto.auto",
-			Title:   title,
-			Message: message,
-			Actions: []toast.Action{
-				{"protocol", "Open", ""},
-				{"protocol", "Cancel", ""},
-			},
-		}
-		err := notification.Push()
-		if err != nil {
-			log.Fatalln(err)
-		}
+
 	} else {
 		note := gosxnotifier.NewNotification(message)
 		note.Title = title
