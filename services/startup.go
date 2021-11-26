0482daf81632d1ed7109ca15b31441b90eae01e5
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-27 04:53:06 +0900

    update mix/max limit

--- services/startup.go
+++ services/startup.go
@@ -12,15 +12,16 @@ import (
 )
 
 var PAIRS = []WatchPair{
-	{"0x3dd49f67e9d5bc4c5e6634b3f70bfd9dc1b6bd74", 7.2, 8.0},   // SAND
+	{"0x3dd49f67e9d5bc4c5e6634b3f70bfd9dc1b6bd74", 7.0, 7.3},   // SAND
 	{"0x7a99822968410431edd1ee75dab78866e31caf39", 0.42, 0.5},  // XI
+	{"0x0d0d65e7a7db277d3e0f5e1676325e75f3340455", 1.3, 1.5},   // MTA
+	{"0x11b1f53204d03e5529f09eb3091939e4fd8c9cf3", 4.5, 5.3},   // MANA
 	{"0x22527f92f43dc8bea6387ce40b87ebaa21f51df3", 1.5, 2.0},   // NUM
 	{"0xc88ac988a655b91b70def427c8778b4d43f2048d", 6.7, 8.0},   // DERC
-	{"0xccb63225a7b19dcf66717e4d40c9a72b39331d61", 9.0, 11.0},  // MC
-	{"0xc0a6bb3d31bb63033176edba7c48542d6b4e406d", 6.0, 8.0},   // RNDR
-	{"0x11b1f53204d03e5529f09eb3091939e4fd8c9cf3", 3.5, 6.0},   // MANA
+	{"0xccb63225a7b19dcf66717e4d40c9a72b39331d61", 8.0, 11.0},  // MC
+	{"0xc0a6bb3d31bb63033176edba7c48542d6b4e406d", 5.0, 8.0},   // RNDR
 	{"0xc8ca3c0f011fe42c48258ecbbf5d94c51f141c17", 2.0, 2.5},   // CGG
-	{"0x4d3138931437dcc356ca511ac812e14ba8199fd6", 0.18, 0.22}, // BONDLY
+	{"0x4d3138931437dcc356ca511ac812e14ba8199fd6", 0.16, 0.22}, // BONDLY
 }
 var oldPrices = map[string]float64{}
 
